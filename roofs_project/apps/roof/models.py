0b33a4ee9b950a86691a923e6dc4e29f8b6b44d7
Victor Chaptsev <chaptsev.ifmo@gmail.com>
2016-04-11 15:29:16 +0200

    Slugify
    Added model function, that slugifies title.
    Pluged transliteration lib.
    Updated requirements.

--- roofs_project/apps/roof/models.py
+++ roofs_project/apps/roof/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.text import slugify
+from transliterate import translit
+
 from .choices import *
 
 
@@ -6,7 +9,6 @@ class Roof(models.Model):
     title = models.CharField(max_length=30)
     slug = models.SlugField(max_length=40)
     metro = models.CharField(max_length=30)
-    address = models.CharField(max_length=100)
     street = models.CharField(max_length=30)
     number = models.PositiveSmallIntegerField()  # TODO: literals
     porch = models.PositiveSmallIntegerField()
@@ -22,7 +24,12 @@ class Roof(models.Model):
     def address(self):
         return '{0}, №{1}, поздъед {2}, этаж {3}'.format(self.street, self.number, self.porch, self.floor)
 
-    # TODO: obtaining coordinates
+    # TODO: slug improvements, if title is not unique
+    def save(self, *args, **kwargs):
+        self.slug = slugify(translit(self.title, reversed=True), allow_unicode=True)
+        super(Roof, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+
+    # TODO: obtaining coordinates
