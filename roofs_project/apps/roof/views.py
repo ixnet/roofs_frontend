272358bc4b341669eaf87594f0ecd37ee98cf0f1
Victor Chaptsev <chaptsev.ifmo@gmail.com>
2016-04-12 12:40:39 +0000

    Design
    x

--- roofs_project/apps/roof/views.py
+++ roofs_project/apps/roof/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
 
-# Create your views here.
+from roof.models import Roof 
+
+def l404(request):
+    return render(request, 
+                  "404.html")
+
+def index(request):
+    return render(request, 
+                  "roof/index.html")
+
+def list(request):
+    queryset = Roof.objects.all()
+    return render(request,
+                  "roof/list.html",
+                  {'list': queryset,
+                   'title': 'Крыши'}
+                  )
+
+
+def detail(request, id=None):
+    instance = get_object_or_404(Roof, id=id)
+    return render(request,
+                  "roof/detail.html",
+                  {'instance': instance,
+                   'title': instance.title}
+                  )
