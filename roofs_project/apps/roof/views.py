ee06cbb55a432b69f1e0a151a2ddceb841cea8ad
Victor Chaptsev <chaptsev.ifmo@gmail.com>
2016-04-13 11:37:59 +0300

    Pagination

--- roofs_project/apps/roof/views.py
+++ roofs_project/apps/roof/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from roof.models import Roof
 
 
@@ -9,7 +9,18 @@ def index(request):
 
 
 def list(request):
-    queryset = Roof.objects.all()
+    queryset_list = Roof.objects.all()
+    # TODO: 10 roofs per page
+    paginator = Paginator(queryset_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+
     return render(request,
                   "roof/list.html",
                   {'list': queryset,
